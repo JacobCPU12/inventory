b58e928f3f07f843bda470256c3af3325dc622ad
JacobCPU12 <s283209@uos.ac.uk>
2025-02-18 12:00:58 +0000

    test files added

--- Class.java
+++ Class.java
@@ -1,8 +1,13 @@
+// Imported Scanner for testing
+import java.util.Scanner;
+
 public class Class {
     public static void main(String[] args) {
         // Your code goes here
         System.out.println("Hello, World!");
         System.out.println("Aloha! How are you doing today?");
         System.out.println("I am good, thanks for asking.");
+
+        // This is just a different comment for testing
     }
 }
