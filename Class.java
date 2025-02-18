0d3fe6d3192b28c31b25b945c6d55942d24bc203
JacobCPU12 <s283209@uos.ac.uk>
2025-02-18 11:50:13 +0000

    change class file with another line

--- Class.java
+++ Class.java
@@ -2,5 +2,6 @@ public class Class {
     public static void main(String[] args) {
         // Your code goes here
         System.out.println("Hello, World!");
+        System.out.println("Aloha! How are you doing today?");
     }
 }
